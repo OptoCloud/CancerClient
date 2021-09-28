6b12c5504a4cb573abb53947b7e2e9700cd532ca
OptoCloud <26223094+OptoCloud@users.noreply.github.com>
2021-09-28 17:15:58 +0200

    Use intptr

--- CancerClient/USpeakNative.cs
+++ CancerClient/USpeakNative.cs
@@ -12,17 +12,19 @@ namespace CancerClient.USpeak
 		[DllImport("USpeakNative.dll", CallingConvention = CallingConvention.Cdecl)]
 		private static extern bool Native_StreamMp3(IntPtr ptr, string path, Int32 pathLength);
 		[DllImport("USpeakNative.dll", CallingConvention = CallingConvention.Cdecl)]
-		private static extern Int32 Native_GetAudioFrame(IntPtr ptr, Int32 playerId, Int32 packetTime, byte[] buffer, Int32 bufferLength);
+		private static extern Int32 Native_GetAudioFrame(IntPtr ptr, Int32 playerId, Int32 packetTime, IntPtr bufferPtr, Int32 bufferLength);
 		[DllImport("USpeakNative.dll", CallingConvention = CallingConvention.Cdecl)]
-		private static extern Int32 Native_RecodeAudioFrame(IntPtr ptr, byte[] dataIn, Int32 dataInLength, byte[] dataOut, Int32 dataOutLength);
+		private static extern Int32 Native_RecodeAudioFrame(IntPtr ptr, IntPtr dataInPtr, Int32 dataInLength, IntPtr bufferPtr, Int32 bufferLength);
 
 		public USpeakNative()
 		{
+			m_bufferGcHandle = GCHandle.Alloc(m_buffer, GCHandleType.Pinned);
 			m_thisPtr = Native_CreateUSpeakLite();
 		}
 		~USpeakNative()
 		{
 			Native_DeleteUSpeakLite(m_thisPtr);
+			m_bufferGcHandle.Free();
 		}
 
 		public bool StreamMp3(string path)
@@ -34,7 +36,7 @@ namespace CancerClient.USpeak
 		{
 			lock (m_lock)
 			{
-				int nRead = Native_GetAudioFrame(m_thisPtr, playerId, packetTime, m_buffer, m_buffer.Length);
+				int nRead = Native_GetAudioFrame(m_thisPtr, playerId, packetTime, m_bufferGcHandle.AddrOfPinnedObject(), BufferSize);
 				if (nRead <= 0)
 				{
 					return null;
@@ -51,7 +53,12 @@ namespace CancerClient.USpeak
 		{
 			lock (m_lock)
 			{
-				int nRead = Native_RecodeAudioFrame(m_thisPtr, dataIn, dataIn.Length, m_buffer, m_buffer.Length);
+				GCHandle dataInHandle = GCHandle.Alloc(m_buffer, GCHandleType.Pinned);
+
+				int nRead = Native_RecodeAudioFrame(m_thisPtr, dataInHandle.AddrOfPinnedObject(), dataIn.Length, m_bufferGcHandle.AddrOfPinnedObject(), BufferSize);
+
+				dataInHandle.Free();
+
 				if (nRead <= 0)
 				{
 					return null;
@@ -64,8 +71,11 @@ namespace CancerClient.USpeak
 			}
 		}
 
+		private const Int32 BufferSize = 1024;
+
 		private readonly IntPtr m_thisPtr;
 		private object m_lock = new object();
-		private byte[] m_buffer = new byte[1022];
+		private byte[] m_buffer = new byte[BufferSize];
+		private GCHandle m_bufferGcHandle;
 	}
 }
